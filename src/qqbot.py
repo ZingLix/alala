a12cb91e236653083354bdbd9ebeef8b0abced1f
ZingLix <me@zinglix.xyz>
2023-06-09 03:12:31 +0000

    bugfix: support bilibili wbi

--- src/qqbot.py
+++ src/qqbot.py
@@ -4,7 +4,7 @@ import requests
 import random
 import re
 import time
-from Util.config import config, req_headers
+from Util.config import config, req_headers, mixinKeyEncTab
 from rule import keywords, rules, bili_mtr_list, filter_sentence
 import json
 from concurrent.futures import ThreadPoolExecutor
@@ -15,6 +15,9 @@ from string import Template
 import datetime
 import logging
 import websocket
+from functools import reduce
+from hashlib import md5
+import urllib.parse
 
 chat_history = {}
 MAX_LEN = config["bot"]["max_msg_len"]
@@ -577,18 +580,61 @@ def command_personal_send_group(text_list, msg):
     return True
 
 
+def getMixinKey(orig: str):
+    "对 imgKey 和 subKey 进行字符顺序打乱编码"
+    return reduce(lambda s, i: s + orig[i], mixinKeyEncTab, "")[:32]
+
+
+def encWbi(params: dict, img_key: str, sub_key: str):
+    "为请求参数进行 wbi 签名"
+    mixin_key = getMixinKey(img_key + sub_key)
+    curr_time = round(time.time())
+    params["wts"] = curr_time  # 添加 wts 字段
+    params = dict(sorted(params.items()))  # 按照 key 重排参数
+    # 过滤 value 中的 "!'()*" 字符
+    params = {
+        k: "".join(filter(lambda chr: chr not in "!'()*", str(v)))
+        for k, v in params.items()
+    }
+    query = urllib.parse.urlencode(params)  # 序列化参数
+    wbi_sign = md5((query + mixin_key).encode()).hexdigest()  # 计算 w_rid
+    params["w_rid"] = wbi_sign
+    return params
+
+
+def getWbiKeys() -> tuple[str, str]:
+    "获取最新的 img_key 和 sub_key"
+    resp = requests.get("https://api.bilibili.com/x/web-interface/nav")
+    resp.raise_for_status()
+    json_content = resp.json()
+    img_url: str = json_content["data"]["wbi_img"]["img_url"]
+    sub_url: str = json_content["data"]["wbi_img"]["sub_url"]
+    img_key = img_url.rsplit("/", 1)[1].split(".")[0]
+    sub_key = sub_url.rsplit("/", 1)[1].split(".")[0]
+    return img_key, sub_key
+
+
 def bili_monitor():
     cfg = config["bot"]["bili_monitor"]
     sleep_time = cfg["interval"]
     headers = req_headers["bili"]
     last_check_time = int(time.time())
     while True:
+        img_key, sub_key = getWbiKeys()
         for item in bili_mtr_list():
             try:
+                params = {
+                    "mid": item["uid"],
+                    "pn": 1,
+                    "ps": 25,
+                    "order": "pubdate",
+                    "index": 1,
+                    "jsonp": "jsonp",
+                }
+                signed_params = encWbi(params, img_key, sub_key)
                 r = requests.get(
-                    "https://api.bilibili.com/x/space/wbi/arc/search?mid={}&pn=1&ps=25&order=pubdate&index=1&jsonp=jsonp".format(
-                        item["uid"]
-                    ),
+                    "https://api.bilibili.com/x/space/wbi/arc/search",
+                    params=signed_params,
                     headers=headers,
                 )
                 r = r.json()
